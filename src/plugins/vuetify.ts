b550733acb04523e6547bba28781a3de52a70f12
Gregor Becker <g.becker@careerpartner.eu>
2020-03-18 21:17:23 +0100

    feat(setup): Add rally preset & fa

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,13 @@
 import '@fortawesome/fontawesome-free/css/all.css';
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import { preset } from 'vue-cli-plugin-vuetify-preset-rally/preset'
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+    preset,
+    icons: {
+        iconfont: 'fa',
+    }
+});
--- src/types/vuetify.d.ts
+++ src/types/vuetify.d.ts
@@ -1,1 +1,4 @@
 declare module 'vuetify/lib/components' {}
+declare module 'vue-cli-plugin-vuetify-preset-rally/preset' {
+    export const preset: any;
+}
